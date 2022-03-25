70a064af990efcbc87ad342d5e37f3a55f25d8e5
mhkhan4 <mhkhan4@buffalo.edu>
2022-03-25 16:42:48 -0400

    creating a website to crack interview questions

---
+++ src/main/java/com/interview/crack_coding_interviews/CrackCodingInterviewsApplication.java
@@ -0,0 +1,13 @@
+package com.interview.crack_coding_interviews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrackCodingInterviewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrackCodingInterviewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/interview/crack_coding_interviews/CrackCodingInterviewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interview.crack_coding_interviews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrackCodingInterviewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
